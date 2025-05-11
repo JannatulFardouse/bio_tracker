1490788f9987b81c4c9eb964ce4f56afabca8be1
JannatulFardouse <jannatul.fardouse.cse@gmail.com>
2025-05-11 13:50:19 +0200

    implement the useEffect and useState hook function for datatable and health chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+import data from './data/healthData.json';
+import DataTable from './components/DataTable';
+import HealthChart from './components/HealthChart';
+
 function App() {
+  const [healthData, setHealthData] = useState([]);
+
+  useEffect(() => {
+    setHealthData(data); // Mock fetch
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>BioTracker - Health Dashboard</h1>
+      <HealthChart data={healthData} />
+      <DataTable data={healthData} />
     </div>
   );
 }
 
 export default App;
+
